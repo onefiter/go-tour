a346c86596aa8444b998d449b6114ffe511cbfab
onefiter <onefiter@gmail.com>
2023-07-22 22:41:25 +0800

    web

--- web/beego/controller_test.go
+++ web/beego/controller_test.go
@@ -6,8 +6,10 @@ import (
 )
 
 func TestUserController(t *testing.T) {
+	// Beego中独有的配置参数
 	web.BConfig.CopyRequestBody = true
 	c := &UserController{}
+	// Get请求
 	web.Router("/user", c, "get:GetUser")
 	web.Run(":8081")
 }
