a468490e6bd86a2fd1f2e2897f74b3c4dd09d70a
onefiter <onefiter@gmail.com>
2023-10-21 16:14:31 +0800

    live concurrent_blocking_queue

--- live/queue/concurrent_blocking_queue.go
+++ live/queue/concurrent_blocking_queue.go
@@ -10,57 +10,84 @@ import (
 type ConcurrentBlockingQueue[T any] struct {
 	mutex *sync.Mutex
 	data  []T
-	//notFull  chan struct{}
-	//notEmpty chan struct{}
+	// notFull chan struct{}
+	// notEmpty chan struct{}
 	maxSize int
 
 	notEmptyCond *Cond
 	notFullCond  *Cond
+
+	count int
+	head  int
+	tail  int
+
+	zero T
 }
 
 func NewConcurrentBlockingQueue[T any](maxSize int) *ConcurrentBlockingQueue[T] {
 	m := &sync.Mutex{}
 	return &ConcurrentBlockingQueue[T]{
-		data:  make([]T, 0, maxSize),
+		// 即便是 ring buffer，一次性分配完内存，也是有缺陷的
+		// 如果不想一开始就把所有的内存都分配好，可以用链表
+		data:  make([]T, maxSize),
 		mutex: m,
-		//notFull:  make(chan struct{}, 1),
-		//notEmpty: make(chan struct{}, 1),
+		// notFull: make(chan struct{}, 1),
+		// notEmpty: make(chan struct{}, 1),
 		maxSize:      maxSize,
 		notFullCond:  NewCond(m),
 		notEmptyCond: NewCond(m),
 	}
 }
 
-func (c *ConcurrentBlockingQueue[T]) Enqueue(ctx context.Context, data T) error {
+// func (c *ConcurrentBlockingQueue[T]) Get(index int) (T, error) {
+// 	index = (c.head + index) % c.maxSize
+// }
 
+func (c *ConcurrentBlockingQueue[T]) EnQueue(ctx context.Context, data T) error {
 	if ctx.Err() != nil {
 		return ctx.Err()
 	}
 	c.mutex.Lock()
-
 	for c.isFull() {
 		err := c.notFullCond.WaitWithTimeout(ctx)
 		if err != nil {
 			return err
 		}
 	}
-	// 1. 缺点append会导致数组不断扩容，出队列没能解决缓存--可以用两个指针进行维护
-	// 2.可能引起扩容
-	c.data = append(c.data, data)
-	c.notFullCond.Broadcast()
-	c.mutex.Unlock()
 
+	// 按需扩容
+	// if len(c.data) < c.maxSize {
+	// 	c.data = append(c.data, data)
+	// 	c.tail ++
+	// 	c.count ++
+	// } else {
+	// 	c.data[c.tail] = data
+	// 	c.tail ++
+	// 	c.count ++
+	// 	if c.tail == c.maxSize {
+	// 		c.tail = 0
+	// 	}
+	// }
+
+	c.data[c.tail] = data
+	c.tail++
+	c.count++
+	if c.tail == c.maxSize {
+		c.tail = 0
+	}
+
+	c.notEmptyCond.Broadcast()
+	c.mutex.Unlock()
+	// 没有人等 notEmpty 的信号，这一句就会阻塞住
 	return nil
 }
 
-func (c *ConcurrentBlockingQueue[T]) Dequeue(ctx context.Context) (T, error) {
-
+func (c *ConcurrentBlockingQueue[T]) DeQueue(ctx context.Context) (T, error) {
 	if ctx.Err() != nil {
 		var t T
 		return t, ctx.Err()
 	}
 	c.mutex.Lock()
-
 	for c.isEmpty() {
 		err := c.notEmptyCond.WaitWithTimeout(ctx)
 		if err != nil {
@@ -68,15 +95,20 @@ func (c *ConcurrentBlockingQueue[T]) Dequeue(ctx context.Context) (T, error) {
 			return t, err
 		}
 	}
-	// 队首
-	// [a, b, c, d]
-	t := c.data[0]
-	c.data = c.data[1:]
-	c.notEmptyCond.Broadcast()
+
+	// 这里要不要考虑缩容？
+
+	t := c.data[c.head]
+	c.data[c.head] = c.zero
+	c.head++
+	c.count--
+	if c.head == c.maxSize {
+		c.head = 0
+	}
+	c.notFullCond.Broadcast()
 	c.mutex.Unlock()
-
+	// 没有人等 notFull 的信号，这一句就会阻塞住
 	return t, nil
-
 }
 
 func (c *ConcurrentBlockingQueue[T]) IsFull() bool {
@@ -86,8 +118,7 @@ func (c *ConcurrentBlockingQueue[T]) IsFull() bool {
 }
 
 func (c *ConcurrentBlockingQueue[T]) isFull() bool {
-
-	return len(c.data) == c.maxSize
+	return c.count == c.maxSize
 }
 
 func (c *ConcurrentBlockingQueue[T]) IsEmpty() bool {
@@ -97,13 +128,33 @@ func (c *ConcurrentBlockingQueue[T]) IsEmpty() bool {
 }
 
 func (c *ConcurrentBlockingQueue[T]) isEmpty() bool {
-	return len(c.data) == 0
+	return c.count == 0
 }
 
 func (c *ConcurrentBlockingQueue[T]) Len() uint64 {
-	return uint64(len(c.data))
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	return uint64(c.count)
 }
 
+// func (c *ConcurrentBlockingQueue[T]) EnQueueV1(ctx context.Context, data T) error {
+// select {
+// case <- c.notFullCond.Wait():
+// 	case <- ctx.Done() :
+//
+// }
+
+// 	c.notFullCond.Wait(timeout)
+// }
+
+// func (c *ConcurrentBlockingQueue[T]) DeQueueV1(ctx context.Context, data T) error {
+// 	c.notFullCond.Signal()
+// 	return nil
+// }
+
+// Conditional variable implementation that uses channels for notifications.
+// Only supports .Broadcast() method, however supports timeout based Wait() calls
+// unlike regular sync.Cond.
 type Cond struct {
 	L sync.Locker
 	n unsafe.Pointer
@@ -116,6 +167,8 @@ func NewCond(l sync.Locker) *Cond {
 	return c
 }
 
+// Waits for Broadcast calls. Similar to regular sync.Cond, this unlocks the underlying
+// locker first, waits on changes and re-locks it before returning.
 func (c *Cond) Wait() {
 	n := c.NotifyChan()
 	c.L.Unlock()
@@ -123,10 +176,10 @@ func (c *Cond) Wait() {
 	c.L.Lock()
 }
 
+// Same as Wait() call, but will only wait up to a given timeout.
 func (c *Cond) WaitWithTimeout(ctx context.Context) error {
 	n := c.NotifyChan()
 	c.L.Unlock()
-
 	select {
 	case <-n:
 		c.L.Lock()
@@ -134,16 +187,16 @@ func (c *Cond) WaitWithTimeout(ctx context.Context) error {
 	case <-ctx.Done():
 		c.L.Lock()
 		return ctx.Err()
-
 	}
 }
 
 // Returns a channel that can be used to wait for next Broadcast() call.
 func (c *Cond) NotifyChan() <-chan struct{} {
 	ptr := atomic.LoadPointer(&c.n)
-	return *(*chan struct{})(ptr)
+	return *((*chan struct{})(ptr))
 }
 
+// Broadcast call notifies everyone that something has changed.
 func (c *Cond) Broadcast() {
 	n := make(chan struct{})
 	ptrOld := atomic.SwapPointer(&c.n, unsafe.Pointer(&n))
--- live/queue/concurrent_blocking_queue_test.go
+++ live/queue/concurrent_blocking_queue_test.go
@@ -2,119 +2,125 @@ package queue
 
 import (
 	"context"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"math/rand"
 	"sync"
 	"testing"
 	"time"
 )
 
-func TestConcurrentBlockingQueue_EnQueue(t *testing.T) {
-	testCases := []struct {
-		name    string
-		q       *ConcurrentBlockingQueue[int]
-		timeout time.Duration
-		value   int
-		data    []int
-		wantErr error
-	}{
-		{
-			name:    "入队成功",
-			q:       NewConcurrentBlockingQueue[int](10),
-			value:   1,
-			timeout: time.Minute,
-			data:    []int{1},
-		},
-		{
-			name: "blocking and timeout",
-			q: func() *ConcurrentBlockingQueue[int] {
-				res := NewConcurrentBlockingQueue[int](2)
-
-				ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-				defer cancel()
-
-				err := res.Enqueue(ctx, 1)
-				require.NoError(t, err)
-				err = res.Enqueue(ctx, 2)
-				require.NoError(t, err)
-				return res
-			}(),
-			value:   3,
-			timeout: time.Second,
-			data:    []int{1, 2},
-			wantErr: context.DeadlineExceeded,
-		},
-	}
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			ctx, cancel := context.WithTimeout(context.Background(), tc.timeout)
-			defer cancel()
-
-			err := tc.q.Enqueue(ctx, tc.value)
-			assert.Equal(t, tc.wantErr, err)
-			assert.Equal(t, tc.data, tc.q.data)
-		})
-	}
-}
+// func TestConcurrentBlockingQueue_EnQueue(t *testing.T) {
+// 	testCases := []struct{
+// 		name string
+//
+// 		q *ConcurrentBlockingQueue[int]
+//
+// 		timeout time.Duration
+// 		value int
+//
+// 		data []int
+//
+// 		wantErr error
+// 	}{
+// 		{
+// 			name: "enqueue",
+// 			q: NewConcurrentBlockingQueue[int](10),
+// 			value: 1,
+// 			timeout: time.Minute,
+// 			data: []int{1},
+// 		},
+// 		{
+// 			name: "blocking and timeout",
+// 			q: func() *ConcurrentBlockingQueue[int]{
+// 				res := NewConcurrentBlockingQueue[int](2)
+// 				ctx, cancel :=context.WithTimeout(context.Background(), time.Second)
+// 				defer cancel()
+// 				err := res.EnQueue(ctx, 1)
+// 				require.NoError(t, err)
+// 				err = res.EnQueue(ctx, 2)
+// 				require.NoError(t, err)
+// 				return res
+// 			}(),
+// 			value: 3,
+// 			timeout: time.Second,
+// 			data: []int{1, 2},
+// 			wantErr: context.DeadlineExceeded,
+// 		},
+// 	}
+//
+// 	for _, tc := range testCases {
+// 		t.Run(tc.name, func(t *testing.T) {
+// 			ctx, cancel := context.WithTimeout(context.Background(), tc.timeout)
+// 			defer cancel()
+// 			err := tc.q.EnQueue(ctx, tc.value)
+// 			assert.Equal(t, tc.wantErr, err)
+// 			assert.Equal(t, tc.data, tc.q.data)
+// 		})
+// 	}
+// }
 
 func TestConcurrentBlockingQueue(t *testing.T) {
-	// 要测试什么场景
-	// 1. 从goroutine数量考虑
-	//  a. enqueue和dequeue数量一样
-	//  b. enqueue > dequeue
-	//  c. enqueue < dequeue
-	// 加锁的话，只能确保没有死锁
+	// 只能确保没有死锁
 	q := NewConcurrentBlockingQueue[int](10000)
+	// data := make(chan int, 10000000000000000000000)
+
+	// 并发的问题都落在 m 上
+	// var m sync.Mutex
+	var wg sync.WaitGroup
+	wg.Add(30)
 	for i := 0; i < 20; i++ {
 		go func() {
-			for {
+			for j := 0; j < 1000; j++ {
+				// 你没有办法校验这里面的中间结果
 				ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-				// 没有办法校验这里面的中间结果
-				_ = q.Enqueue(ctx, rand.Int())
-				// 怎么断言error
+				// m.Lock()
+				val := rand.Int()
+				_ = q.EnQueue(ctx, val)
+				// 怎么断言 error
+				// data <- val
+				// m.Unlock()
 				cancel()
-
 			}
+			wg.Done()
 		}()
 	}
 
-	for i := 0; i < 20; i++ {
+	for i := 0; i < 10; i++ {
 		go func() {
-			for {
+			for j := 0; j < 1000; j++ {
 				ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-
-				_, _ = q.Dequeue(ctx)
-				// 怎么断言 val ,err
+				_, _ = q.DeQueue(ctx)
+				// 怎么断言 error
 				cancel()
-
 			}
+			wg.Done()
 		}()
 	}
-	// 怎么校验q对不对
+
+	// 怎么校验 q 对还是不对
+	wg.Wait()
 }
 
+// 切片实现
+// BenchmarkConcurrentQueue-12       100000               306.7 ns/op           223 B/op          4 allocs/op
+
+// BenchmarkConcurrentQueue-12       100000               280.8 ns/op           208 B/op          4 allocs/op
 func BenchmarkConcurrentQueue(b *testing.B) {
 	var wg sync.WaitGroup
-
 	q := NewConcurrentBlockingQueue[int](100)
-
 	wg.Add(2)
-
+	b.ResetTimer()
 	go func() {
 		for i := 0; i < b.N; i++ {
-			_ = q.Enqueue(context.Background(), i)
+			_ = q.EnQueue(context.Background(), i)
 		}
 		wg.Done()
 	}()
 
 	go func() {
 		for i := 0; i < b.N; i++ {
-			_, _ = q.Dequeue(context.Background())
-
+			_, _ = q.DeQueue(context.Background())
 		}
 		wg.Done()
 	}()
-
 	wg.Wait()
 }
